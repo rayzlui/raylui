34ad1972362fecc54e27f1d57a8cd0f4b67d4876
Ray <rayzlui@gmail.com>
2020-01-18 23:42:21 -0800

    add projects to iframe

--- src/views/ArcadeView.js
+++ src/views/ArcadeView.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function ArcadeView(props) {
+export function ArcadeView() {
+  let [currentApp, changeApp] = useState('weather');
+
+  let apps = {
+    weather: ['a fun weather app', 'https://rayzlui.github.io/weather-app/'],
+    pokedex: [
+      'a pocket monsters directory',
+      'https://rayzlui.github.io/pokedex/',
+    ],
+  };
+  let viewing = apps[currentApp];
+  let frame = <iframe title={viewing[0]} src={viewing[1]}></iframe>;
+
   return (
     <section id={'arcade__view'}>
-      <h1>This is Arcade Header</h1>
+      <h1>Apps I Have Built</h1>
+      <nav className={'arcade__nav'}>
+        <button
+          className={'arcade__button'}
+          onClick={() => changeApp('weather')}
+        >
+          Weather
+        </button>
+        <button
+          className={'arcade__button'}
+          onClick={() => changeApp('pokedex')}
+        >
+          Pokedex
+        </button>
+      </nav>
+      {frame}
     </section>
   );
 }
