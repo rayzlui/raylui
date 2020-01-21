c162c270468525f788f124cf711daf0188b28cab
Ray <rayzlui@gmail.com>
2020-01-21 15:49:57 -0800

    Updates

---
+++ precache-manifest.128161d43a8684d786e88ef37672d794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719d21c5127fb8b99ce11c42961e8b4c",
+    "url": "/raylui/index.html"
+  },
+  {
+    "revision": "f13519e24dc0a037800d",
+    "url": "/raylui/static/css/main.e467be56.chunk.css"
+  },
+  {
+    "revision": "948c52d5a5300e4c6eff",
+    "url": "/raylui/static/js/2.667e64b2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/raylui/static/js/2.667e64b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f13519e24dc0a037800d",
+    "url": "/raylui/static/js/main.211fded0.chunk.js"
+  },
+  {
+    "revision": "f5ed1b965cdc4f186681",
+    "url": "/raylui/static/js/runtime-main.250db787.js"
+  }
+]);
