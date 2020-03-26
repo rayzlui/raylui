f160ede9195c116ad2cc119175fe36626913f515
Ray <rayzlui@gmail.com>
2020-03-25 22:14:06 -0700

    move nav to own component

--- src/views/ArcadeView.js
+++ src/views/ArcadeView.js
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
+import { NavBar } from './NavBar';
+
+const apps = {
+  weather: [
+    'a fun weather app',
+    'https://rayzlui.github.io/weather-app/',
+    'https://github.com/rayzlui/weather-app',
+  ],
+  pokedex: [
+    'a pocket monsters directory',
+    'https://rayzlui.github.io/pokedex/',
+    'https://github.com/rayzlui/pokedex',
+  ],
+  battleship: [
+    'battleship game',
+    'https://rayzlui.github.io/battleship/',
+    'https://github.com/rayzlui/battleship',
+  ],
+  chess: [
+    'chess game',
+    'https://rayzlui.github.io/chess/',
+    'https://github.com/rayzlui/chess',
+  ],
+};
 
 export function ArcadeView() {
   let [currentApp, changeApp] = useState('weather');
-
-  let apps = {
-    weather: [
-      'a fun weather app',
-      'https://rayzlui.github.io/weather-app/',
-      'https://github.com/rayzlui/weather-app',
-    ],
-    pokedex: [
-      'a pocket monsters directory',
-      'https://rayzlui.github.io/pokedex/',
-      'https://github.com/rayzlui/pokedex',
-    ],
-    battleship: [
-      'battleship game',
-      'https://rayzlui.github.io/battleship/',
-      'https://github.com/rayzlui/battleship',
-    ],
-    chess: [
-      'chess game',
-      'https://rayzlui.github.io/chess/',
-      'https://github.com/rayzlui/chess',
-    ],
-  };
   let viewing = apps[currentApp];
   let frame = <iframe title={viewing[0]} src={viewing[1]}></iframe>;
 
@@ -33,32 +34,7 @@ export function ArcadeView() {
       <header>
         <h1>Portfolio</h1>
       </header>
-      <nav className={'arcade__nav'}>
-        <button
-          className={'arcade__button'}
-          onClick={() => changeApp('weather')}
-        >
-          Weather
-        </button>
-        <button
-          className={'arcade__button'}
-          onClick={() => changeApp('pokedex')}
-        >
-          Pokedex
-        </button>
-        <button
-          className={'arcade__button'}
-          onClick={() => changeApp('battleship')}
-        >
-          Battleship
-        </button>
-        <button className={'arcade__button'} onClick={() => changeApp('chess')}>
-          Chess
-        </button>
-        <a className={'arcade__button'} href={'#home__view'}>
-          This Site!
-        </a>
-      </nav>
+      <NavBar changeApp={changeApp} />
       {frame}
       <a
         className={'arcade__code__anchor'}
